7a9614650cf6245e7e9745db9b8d5202b4d5c7b0
luckymore <njuluckwang@gmail.com>
2016-10-20 15:30:45 +0800

    api about courses

--- app/course_plus/course.py
+++ app/course_plus/course.py
@@ -9,6 +9,43 @@ import json
 import datetime
 
 
+
+class Author(db.Model):
+    __tablename__ = 't_author'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(16), unique=True)
+    icon = db.Column(db.String(16))
+    avatar = db.Column(db.String(16))
+    phone = db.Column(db.String(16))
+    email = db.Column(db.String(64))
+    qq = db.Column(db.String(16))
+    wechat = db.Column(db.String(32))
+    resourceKey = db.Column(db.String(16))
+    introduction = db.Column(db.Text)
+    createdAt = db.Column(db.DateTime)
+    updateAt = db.Column(db.DateTime)
+    deleteAt = db.Column(db.DateTime)
+    topics = relationship("Topic", backref = "Author")
+
+    def courses(self):
+        topics = Topic.query.filter(Topic.authorId == self.id);
+        course_ids = []
+        for topic in topics:
+            if topic.courseId not in course_ids:
+                course_ids.append(topic.courseId)
+        courses = []
+        for courseId in course_ids:
+            course = Course.query.get(courseId)
+            courses.append(course.briefJson())
+        return courses;
+
+    def simpleJson(self):
+        return {"name":self.name,"icon":self.icon,"id":self.id}
+    def json(self):
+        return {"name":self.name,"avatar":self.avatar,"id":self.id,"description":self.introduction,"resourceKey":self.resourceKey,"courses":self.courses()}
+
+    
+
 class School(db.Model):
     __tablename__ = 't_school'
     id = db.Column(db.Integer, primary_key=True)
@@ -40,24 +77,85 @@ class Course(db.Model):
     updateAt = db.Column(db.DateTime)
     deleteAt = db.Column(db.DateTime)
     specialityId = db.Column(db.Integer, ForeignKey('t_speciality.id'))
+    topics = relationship("Topic", backref = "Course")
+    resources = relationship("Resource", backref = "Course")
 
     def topicNum(self):
-        return 0
+        count = Topic.query.filter(Topic.courseId == self.id).count()
+        return count
+
+    def topics(self):
+        topics = Topic.query.filter(Topic.courseId == self.id)
+        topicsDic = {}
+        for topic in topics:
+            authorId = topic.authorId
+            if (topicsDic.has_key(authorId)):
+                topicsDic[authorId].append(topic.json())
+            else:
+                topicsDic[authorId] = [topic.json()]
+        return topicsDic
+
 
     def resourceNum(self):
-        return 0
+        count = Resource.query.filter(Resource.courseId == self.id).count()
+        return count
+
+    def resources(self):
+        resourcesJson = []
+        resources = Resource.query.filter(Resource.courseId == self.id)
+        for resource in resources:
+            resourcesJson.append(resource.json())
+        return resourcesJson
+
 
     def schoolName(self):
-        return "南京大学"
+        speciality = Speciality.query.get(self.specialityId)
+        school = School.query.get(speciality.schoolId)
+        return school.name
 
     def specialityName(self):
         speciality = Speciality.query.get(self.specialityId)
         if speciality:
             return speciality.name
         return ""
-
+    
     def authors(self):
-        return []
+        topics = Topic.query.filter(Topic.courseId == self.id)
+        author_ids = []
+        for topic in topics:
+            if topic.authorId not in author_ids :
+                author_ids.append(topic.authorId)
+        authors = []
+        for authorId in author_ids:
+            author = Author.query.get(authorId)
+            authors.append(author.simpleJson())
+        return authors
+    
+    def detail_authors(self):
+        topics = Topic.query.filter(Topic.courseId == self.id)
+        author_ids = []
+        for topic in topics:
+            if topic.authorId not in author_ids :
+                author_ids.append(topic.authorId)
+        authors = []
+        for authorId in author_ids:
+            author = Author.query.get(authorId)
+            authors.append(author.json())
+        return authors
+
+    def json(self):
+        return {"id":self.id,
+        "name":self.name,
+        "topicNum":self.topicNum(),
+        "resourceNum":self.resourceNum(),
+        "schoolName":self.schoolName(),
+        "specialityName":self.specialityName(),
+        "specialityId":self.specialityId,
+         "description":self.introduction,
+        "resources":self.resources(),
+        "topics":self.topics(),
+        "authors":self.detail_authors()
+        }
 
     def simpleJson(self):
         return {"id":self.id,
@@ -68,6 +166,39 @@ class Course(db.Model):
         "specialityName":self.specialityName(),
         "specialityId":self.specialityId,
         "authors":self.authors()}
+    
+    def briefJson(self):
+        return {"id":self.id,
+        "name":self.name}
+
+
+class Topic(db.Model):
+    __tablename__ = 't_topic'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(16))
+    createdAt = db.Column(db.DateTime)
+    updateAt = db.Column(db.DateTime)
+    deleteAt = db.Column(db.DateTime)
+    courseId = db.Column(db.Integer, ForeignKey('t_course.id'))
+    authorId = db.Column(db.Integer, ForeignKey('t_author.id'))
+
+    def json(self):
+        return {"name":self.name,"authorId":self.authorId,"id":self.id}
+
+
+class Resource(db.Model):
+    __tablename__ = 't_attachment'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32))
+    key = db.Column(db.String(32))
+    ext = db.Column(db.String(16))
+    createdAt = db.Column(db.DateTime)
+    updateAt = db.Column(db.DateTime)
+    deleteAt = db.Column(db.DateTime)
+    courseId = db.Column(db.Integer, ForeignKey('t_course.id'))
+
+    def json(self):
+        return {"id":self.id,"key":self.key,"name":self.name,"ext":self.ext}
 
 
 @app.route('/api/web/course/speciality', methods=['GET'])
@@ -103,7 +234,20 @@ def getCourseList():
     print(courseJsonList)
     return (json.dumps(courseJsonList),200)
 
+@app.route('/api/web/course/courseDetail', methods=['GET'])
+def getCourseDetail():
+    course_id = int(request.args.get("id"))
+    if not course_id:
+        abort(400)
+    course = Course.query.get(course_id)
+    if not course:
+        return (jsonify(SimpleResult(-1,"课程不存在").json()),200)
+    else:
+        return (jsonify(course.json()),200)
 
 
 
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify(SimpleResult(-1,"缺少参数").json()), 400)
 
